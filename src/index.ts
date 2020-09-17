48a12580b649697769878b9abb40504d46c8b1dc
Alexander Georgiev <alexfromsofia@gmail.com>
2020-09-17 18:36:10 +0300

    :construction_worker: Add Mikrom for db connectivity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("object");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+
+  const post = orm.em.create(Post, { title: "My first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
