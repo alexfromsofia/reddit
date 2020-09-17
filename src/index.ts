7a5a8781fedfb7824eda43a9422f107cee0d62e9
Alexander Georgiev <alexfromsofia@gmail.com>
2020-09-17 19:35:26 +0300

    :boom: Setup express and graphql servers

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
+import express from "express";
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   // Connect to DB
   const orm = await MikroORM.init(mikroOrmConfig);
   // Run migrations
   await orm.getMigrator().up();
-  // Run SQL
-  // const post = orm.em.create(Post, { title: "My first post" });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  // Setup express server
+  const app = express();
+
+  // Setup GraphQL server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
