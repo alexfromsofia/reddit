c50724c738d38644faf45c9a43e2afd6b8a6344e
Alexander Georgiev <alexfromsofia@gmail.com>
2020-09-25 10:23:10 +0300

    :heavy_plus_sign: Add nextjs and a simple register form

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface RegisterProps {}
+
+export const Register: React.FC<RegisterProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="Username"
+            />
+            <Box mt="4">
+              <InputField
+                name="password"
+                label="Password"
+                placeholder="Password"
+                type="password"
+              />
+            </Box>
+            <Button mt="4" isLoading={isSubmitting} type="submit">
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
