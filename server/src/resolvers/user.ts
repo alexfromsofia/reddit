9ac38e3f180d8c6998407100dc8ebc21875135a4
Alexander Georgiev <alexfromsofia@gmail.com>
2020-09-28 13:23:13 +0300

    :bulb: Use KnexQuery for creating a user

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 } from "type-graphql";
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
@@ -66,13 +67,20 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
 
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (error) {
       if (error.code === "23505") {
         // Duplicate username code
